6f87b989236e570e6ba98f0bfd7adccb60561061
chenfangxu <fangxu.chen@17zuoye.com>
2022-05-06 19:11:00 +0800

    feat: optimized commitlint configuration templale

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,37 +1,50 @@
 "use strict";
 // .commitlintrc.js
 /** @type {import('cz-git').UserConfig} */
+var commitTypes = [
+    { value: "feat", name: "feat:     A new feature", emoji: ":sparkles:" },
+    { value: "fix", name: "fix:      A bug fix", emoji: ":bug:" },
+    { value: "docs", name: "docs:     Documentation only changes", emoji: ":memo:" },
+    { value: "style", name: "style:    Changes that do not affect the meaning of the code", emoji: ":lipstick:" },
+    {
+        value: "refactor",
+        name: "refactor: A code change that neither fixes a bug nor adds a feature",
+        emoji: ":recycle:",
+    },
+    { value: "perf", name: "perf:     A code change that improves performance", emoji: ":zap:" },
+    { value: "test", name: "test:     Adding missing tests or correcting existing tests", emoji: ":white_check_mark:" },
+    {
+        value: "build",
+        name: "build:    Changes that affect the build system or external dependencies",
+        emoji: ":building_construction:",
+    },
+    { value: "ci", name: "ci:       Changes to our CI configuration files and scripts", emoji: ":green_heart:" },
+    { value: "chore", name: "chore:    Other changes that don't modify src or test files", emoji: ":hammer:" },
+    { value: "revert", name: "revert:   Reverts a previous commit", emoji: ":rewind:" },
+];
 module.exports = {
     rules: {
-    // @see: https://commitlint.js.org/#/reference-rules
-
+        // @see: https://commitlint.js.org/#/reference-rules
+        "type-empty": [2, "never"],
+        "type-case": [2, "always", "lower-case"],
+        "type-enum": [2, "always", commitTypes.map(function (type) { return type.value; })],
+        "scope-case": [2, "always", "lower-case"],
+        "subject-empty": [2, "never"],
     },
     prompt: {
         messages: {
-            type: "选择你要提交的类型 :",
-            scope: "选择一个提交范围（可选）:",
-            customScope: "请输入自定义的提交范围 :",
-            subject: "填写简短精炼的变更描述 :\n",
-            body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
-            breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
-            footerPrefixsSelect: "选择关联issue前缀（可选）:",
-            customFooterPrefixs: "输入自定义issue前缀 :",
-            footer: "列举关联issue (可选) 例如: #31, #I3244 :\n",
-            confirmCommit: "是否提交或修改commit ?",
+            type: "Select the type of change that you're committing:",
+            scope: "Denote the SCOPE of this change (optional):",
+            customScope: "Denote the SCOPE of this change:",
+            subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
+            body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+            breaking: 'List any BREAKING CHANGES (optional). Use "|" to break new line:\n',
+            footerPrefixsSelect: "Select the ISSUES type of changeList by this change (optional):",
+            customFooterPrefixs: "Input ISSUES prefix:",
+            footer: "List any ISSUES by this change. E.g.: #31, #34:\n",
+            confirmCommit: "Are you sure you want to proceed with the commit above?",
         },
-        types: [
-            { value: "feat", name: "feat:     新增功能 | A new feature" },
-            { value: "fix", name: "fix:      修复缺陷 | A bug fix" },
-            { value: "docs", name: "docs:     文档更新 | Documentation only changes" },
-            { value: "style", name: "style:    代码格式 | Changes that do not affect the meaning of the code" },
-            { value: "refactor", name: "refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature" },
-            { value: "perf", name: "perf:     性能提升 | A code change that improves performance" },
-            { value: "test", name: "test:     测试相关 | Adding missing tests or correcting existing tests" },
-            { value: "build", name: "build:    构建相关 | Changes that affect the build system or external dependencies" },
-            { value: "ci", name: "ci:       持续集成 | Changes to our CI configuration files and scripts" },
-            { value: "revert", name: "revert:   回退代码 | Revert to a commit" },
-            { value: "chore", name: "chore:    其他修改 | Other changes that do not modify src or test files" },
-        ],
+        types: commitTypes,
         useEmoji: false,
         scopes: [],
         allowCustomScopes: true,
@@ -43,12 +56,8 @@ module.exports = {
         allowBreakingChanges: ["feat", "fix"],
         breaklineNumber: 100,
         breaklineChar: "|",
-        skipQuestions: [],
-        issuePrefixs: [
-            // 如果使用 gitee 作为开发管理
-            { value: "link", name: "link:     链接 ISSUES 进行中" },
-            { value: "closed", name: "closed:   标记 ISSUES 已完成" },
-        ],
+        skipQuestions: ["breaking", "footerPrefix"],
+        issuePrefixs: [{ value: "closed", name: "closed:   ISSUES has been processed" }],
         customIssuePrefixsAlign: "top",
         emptyIssuePrefixsAlias: "skip",
         customIssuePrefixsAlias: "custom",
--- src/init/husky.ts
+++ src/init/husky.ts
@@ -9,7 +9,7 @@ function init(options: CommanderOptions) {
   const huskyDirPath = path.join(process.cwd(), ".husky");
   const isInstalledHusky = fs.existsSync(huskyDirPath);
   const isForce = options.force;
-  if (!isInstalledHusky) {
+  if (!isInstalledHusky || isForce) {
     execSync("npx husky-init && npm install");
   }
   updateHooks(huskyDirPath, isForce);
@@ -19,34 +19,26 @@ function updateHooks(rootDir: string, isForce: boolean) {
   const prepareCommitMsgPath = path.join(rootDir, "prepare-commit-msg");
   const commitMsgPath = path.join(rootDir, "commit-msg");
 
-  if (fs.existsSync(prepareCommitMsgPath)) {
-    if (isForce) {
-      console.log(chalk.yellow(`using --force，the .husky/prepare-commit-msg will override.`));
-      fs.removeSync(prepareCommitMsgPath);
-    } else {
-      console.log(
-        chalk.yellow(
-          `The file already exists: .husky/prepare-commit-msg, husky is append, not override.If you want to override hooks file, please use Use --force to override.`,
-        ),
-      );
-    }
+  if (fs.existsSync(prepareCommitMsgPath) && isForce) {
+    console.log("using --force，the .husky/prepare-commit-msg will override.");
+    fs.removeSync(prepareCommitMsgPath);
+  } else {
+    console.log(
+      "The file already exists: .husky/prepare-commit-msg, husky is append, not override.If you want to override hooks file, please use Use --force to override.",
+    );
   }
-  if (fs.existsSync(commitMsgPath)) {
-    if (isForce) {
-      console.log(chalk.yellow(`using --force，the .husky/commit-msg will override.`));
-      fs.removeSync(commitMsgPath);
-    } else {
-      console.log(
-        chalk.yellow(
-          `he file already exists: .husky/commit-msg, husky is append , not override.If you want to override hooks file, please use Use --force to override.`,
-        ),
-      );
-    }
+  if (fs.existsSync(commitMsgPath) && isForce) {
+    console.log("using --force，the .husky/commit-msg will override.");
+    fs.removeSync(commitMsgPath);
+  } else {
+    console.log(
+      "The file already exists: .husky/commit-msg, husky is append , not override.If you want to override hooks file, please use Use --force to override.",
+    );
   }
 
   // TODO 添加一些导语
   execSync("npx husky add .husky/prepare-commit-msg 'exec < /dev/tty && npx cz --hook || true'");
-  execSync("npx husky add .husky/commit-msg 'npx commitlint --edit \"$1\"'");
+  execSync("npx husky add .husky/commit-msg 'npx --no -- commitlint --edit \"$1\"'");
 }
 
 export default init;
